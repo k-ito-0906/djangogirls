eec78a13761f55d0e8135499a9358dff3bd209a6
Ito Koki <enpitsu_2005@icloud.com>
2025-12-20 19:20:10 +0900

    ポスト機能の追加

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
-from .models import Post  # 倉庫(モデル)からPostを呼び出す
+from django.shortcuts import render, get_object_or_404, redirect # redirectを追加
+from .models import Post
+from .forms import PostForm # これを追加
+from django.utils import timezone
 
-def post_list(request):
-    # 倉庫から記事を全部取ってくる（QuerySetと言います）
-    posts = Post.objects.all() 
-    # blog/post_list.html という「お皿」に posts を載せてお客さんに返す
-    return render(request, 'blog/post_list.html', {'posts': posts})
+# ...他のビューはそのまま...
+
+def post_new(request):
+    if request.method == "POST":
+        # 保存ボタンが押されたとき
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        # 最初に見る時（空のフォームを表示）
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
